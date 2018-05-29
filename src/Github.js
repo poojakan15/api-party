9192ff9fb5aa409ab49b5d99dbd4e05c565eba5e
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-29 11:16:42 -0400

    Added: route for matching username params

--- src/Github.js
+++ src/Github.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './Github.css'
+import { Route } from 'react-router-dom'
 
 class Github extends Component {
     state = {
@@ -24,7 +25,7 @@ class Github extends Component {
                     src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
                     alt="GitHub"
                 />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                             type="text" 
@@ -36,6 +37,8 @@ class Github extends Component {
                         <button type="submit">Look up GitHub user</button>
                     </div>
                 </form>
+                <Route path="/github/:username" render={(props) => <h3>You searched for {props.match.params.username}</h3>} />
+                <Route exact path="/github" render={() => <h3>Please enter username to search on GitHub</h3>} />
             </div>
         )
     }
