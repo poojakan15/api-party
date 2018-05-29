dca6c7ff3dcc8130f957d3cdc7ac2e2de3204ce9
Pooja Kanuri <pk.musikluvr@gmail.com>
2018-05-29 11:05:19 -0400

    Added: github component to form, value of input to state

---
+++ src/Github.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './Github.css'
+
+class Github extends Component {
+    state = {
+        username: '',
+    }
+
+    handleChange = (ev) => {
+        this.setState({ username: ev.target.value }) 
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+    }
+ 
+    render () {
+        return (
+            <div className="Github">
+                <img
+                    className="logo"
+                    src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+                    alt="GitHub"
+                />
+                <form>
+                    <div>
+                        <input 
+                            type="text" 
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Look up GitHub user</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Github
